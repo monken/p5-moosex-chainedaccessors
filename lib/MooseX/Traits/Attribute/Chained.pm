de5ad5c4909a6922f62e867246db6597f12734c8
Moritz Onken <onken@netcubed.de>
2011-03-01 10:59:17 +0100

    moose2 compat

---
+++ lib/MooseX/Traits/Attribute/Chained.pm
@@ -0,0 +1,42 @@
+package MooseX::Traits::Attribute::Chained;
+use Moose::Role;
+use MooseX::ChainedAccessors::Accessor;
+
+sub accessor_metaclass { 'MooseX::ChainedAccessors::Accessor' }
+
+no Moose::Role;
+1;
+
+
+__END__
+
+=head1 NAME
+
+MooseX::Traits::Attribute::Chained - Create method chaining attributes
+
+=head1 SYNOPSIS
+
+    has => 'debug' => (
+        traits => [ 'Chained' ],
+        is => 'rw',
+        isa => 'Bool',
+    );
+    
+=head1 DESCRIPTION
+
+Modifies the Accessor Metaclass to use MooseX::ChainedAccessors::Accessor
+
+=head1 AUTHORS
+
+Moritz Onken E<lt>onken@netcubed.deE<gt>
+
+David McLaughlin E<lt>david@dmclaughlin.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 David McLaughlin
+
+This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+
+
